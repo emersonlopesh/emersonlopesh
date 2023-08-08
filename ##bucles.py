f174a39813b36adf2607153c4f95051ee666fcb8
emersonlopesh <135035122+emersonlopesh@users.noreply.github.com>
2023-08-08 10:18:06 -0500

    Add files via upload

---
+++ ##bucles.py
@@ -0,0 +1,28 @@
+##bucles
+condicion=12==12
+while condicion:
+    print('hola')
+
+
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(condicion)
+
+        hacer un programa donde pida al usuario su edad si su edad no es igual a 20 que siga pidiendo
+
+        edad = 0
+while edad != 20:
+    edad = int(input("Ingresa tu edad: "))
+    
+print("Tu edad es 20.")
+hacer un programa donde pida el nombre yoni si el nombr no es igual que siga pidiendo
+
+nombre = ""
+while nombre != "yoni":
+    nombre = input("Por favor, ingresa el nombre 'yoni': ")
+
+
+print("¡Hola yoni!")
+
+crear un juego donde se debe adivinar un numero
+
